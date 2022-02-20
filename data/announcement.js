092ae10a67b82c2bb6c5e8bc4d83ed0a0ea3c9ef
cheiw256 <chiew256@gmail.com>
2022-02-21 01:22:07 +0800

    Latest Announcements

--- data/announcement.js
+++ data/announcement.js
@@ -4,7 +4,7 @@ const announcement = [
     text: `PLN 2022 registration is open from 21 February 2022 to 13 March 2022 (dates are subject to change)`,
   },
   {
-    title: "Competitive Programming Workshop Registration！",
+    title: "Competitive Programming Workshop Registration!",
     text: `Competitive Programming Workshop registration is open from 21st February 2022 to 4th March 2022.`,
   },
 ];
