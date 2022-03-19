b60d789ac65aa2f09ced50ef63f5aa1d3f8dffbe
Yeow Zi Qin <ziqinyeow@gmail.com>
2022-03-19 11:19:01 +0800

    update style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 
 module.exports = {
   mode: "jit",
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
